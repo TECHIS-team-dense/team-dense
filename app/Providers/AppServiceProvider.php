9cf49d5eae61f01050d8aadfc20651c85a995fe7
96neko3213kyd@gmail.com <96neko3213kyd@gmail.com>
2022-12-29 17:43:14 +0900

    3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         
         //
-        if (\App::environment(['production']) || \APP::environment(['develop'])) {
+        if (\App::environment(['production']) || \APP::environment(['heroku3'])) {
             \URL::forceScheme('https');
         Schema::defaultStringLength(191);
         }
