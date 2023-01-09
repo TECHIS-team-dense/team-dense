a869588195c0461a5de5c35520d7dfa420218d23
96neko3213kyd@gmail.com <96neko3213kyd@gmail.com>
2023-01-09 16:46:41 +0900

    33

--- database/migrations/2022_12_10_143345_add_role_column_to_usertable.php
+++ database/migrations/2022_12_10_143345_add_role_column_to_usertable.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleColumnToUsertable extends Migration
+/* class AddRoleColumnToUsertable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    /* public function up()
     {
         Schema::table('users', function (Blueprint $table) {
             $table->integer('role')->default(0)->comment('0は一般ユーザー、1は管理者');  //カラム追加
@@ -23,10 +23,10 @@ class AddRoleColumnToUsertable extends Migration
      *
      * @return void
      */
-    public function down()
+    /* public function down()
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('role');  //カラムの削除
         });
     }
-}
+} 
